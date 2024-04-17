bc877d51c25a62b75a55cfc7f321caf6a339d873
chenxudong <839823170@qq.com>
2024-04-17 09:01:50 +0800

    ci: 配置tailwindcss与配置postcss.config

---
+++ packages/tailwind-config/tailwind-config.js
@@ -0,0 +1,11 @@
+// import type { Config } from "tailwindcss";
+
+const config = {
+  content: [
+    './src/**/*.{vue,js,ts,jsx,tsx,mdx}',
+    'node_modules/ql-styles/**/*.{vue,jsx,tsx}'
+  ]
+  // ... 其他配置
+}
+
+export default config
